c6fd859d4ba1f5b043cb02466fbb9b2143e8e157
caitlinbou <caitlin.bouroncle@gmail.com>
2020-09-20 14:18:10 -0700

    basic html layout and API pull data added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # weatherDash
+
+current and 5 day future forecast
+-current has name, date, icon rep of conditions, temp in f, humidity, wind speed, uv index
+----uv index (hover or click?) shows color indicator of favorable, moderate, severe
+----5 day future showsdate, icon representation of conditions, temp and humidity
+
+cities searched for saved in local storage, when clicked on gets updated conditions. 
+
+when dashsboard opened, last searched city forecast populates.
--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("I like blue")
+var APIKey = "80555cce8f2ab40520d4836bf43096e6";
+var city = "seattle";
+var queryURLnow = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+var queryURLforecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
+
+$.ajax({
+  url: queryURLnow,
+  method: "GET",
+}).then(function (responseNow) {
+  console.log(responseNow);
+var tempK = responseNow.main.temp;
+var tempF = Math.round(((9/5)*(tempK - 273) + 32))
+var humidity = responseNow.main.humidity;
+var wind = responseNow.wind.speed;
+var lat = responseNow.coord.lat;
+var lon = responseNow.coord.lon;
+var icon = responseNow.weather.icon;
+var queryURLuv = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`;
+
+$.ajax({
+    url: queryURLuv,
+    method: "GET",
+  }).then(function (responseUV) {
+    console.log(responseUV);
+    var UVindex = responseUV.value
+    console.log(
+        "Temperature: " + tempF,
+        "Humidity: " + humidity,
+        "Wind Speed: " + wind,
+        "lat: " + lat,
+        "lon: " + lon,
+        "UV Index: " + UVindex,
+        icon
+      );
+  });
+});
+$.ajax({
+    url: queryURLforecast,
+    method: "GET",
+}).then(function (responseForecast){
+    console.log(responseForecast)
+    var day1 = ""
+    var day2 = ""
+    var day3 = ""
+    var day4 = ""
+    var day5 = ""
+})
+
+
+
+
+
+
+// function logData(city){
+
+// }
