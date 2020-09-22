62095083a6b4ff46b7e83c4321ede89e79653596
caitlinbou <caitlin.bouroncle@gmail.com>
2020-09-22 10:18:34 -0700

    desired features functioning

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # weatherDash
 
-current and 5 day future forecast
--current has name, date, icon rep of conditions, temp in f, humidity, wind speed, uv index
-----uv index (hover or click?) shows color indicator of favorable, moderate, severe
-----5 day future showsdate, icon representation of conditions, temp and humidity
+This is a basic weather dashboard application. 
 
-cities searched for saved in local storage, when clicked on gets updated conditions. 
+WHEN user inputs a city value
+THEN user recieves weather information for current day and the five-day forecast.
+Recent Search data is stored in localStorage and shown on the page for up to TODO: cities, and when clicked the updated conditions for that city are shown.
+The safety of the UV index for current day is show in green for favorable, orange for moderate, and red for severe.
+The units of measurement are imperial, so it shows temperature in degrees farenheit, and wind in MPH.
 
-when dashsboard opened, last searched city forecast populates.
+when the dashsboard is re-opened by user after initila use, the most recently searched city will show.
+
+To build the structure of this application, I used bootstrap.
--- script.js
+++ script.js
@@ -1,68 +1,138 @@
 var APIKey = "80555cce8f2ab40520d4836bf43096e6";
-var city = "seattle";
-var queryURLnow = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
-// var queryURLforecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
+var city = "";
+var cities =[];
+var header;
+function displayWeatherInfo(){
 
-$.ajax({
-  url: queryURLnow,
-  method: "GET",
-}).then(function (responseNow) {
-  console.log(responseNow);
-  var lat = responseNow.coord.lat;
-  var lon = responseNow.coord.lon;
-  var queryURLall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${APIKey}&units=imperial`;
-
-  $.ajax({
-    url: queryURLall,
+    var city = $(".citySearch").val().trim()
+    var queryURLnow = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
+    $.ajax({
+    url: queryURLnow,
     method: "GET",
-  }).then(function (responseAll) {
-    console.log(queryURLall);
-    console.log(responseAll);
-    var cityName = responseNow.name;
-    var dateToday = responseNow.dt;
-    var tempF = responseAll.current.temp;
-    var humidity = responseAll.current.humidity;
-    var wind = responseAll.current.wind_speed;
-    var uvIndex = responseAll.current.uvi;
-    var icon = responseNow.weather.icon;
-    var forecast = responseAll.daily;
-    console.log(forecast);
-    for (i = 1; i < 6; i++) {
-      console.log(
-        // TODO:convert date to standard date
-        "date: " + forecast[i].dt,
-        "minimum temp: " + forecast[i].temp.min,
-        "maximum temp: " + forecast[i].temp.max,
-        "humidity: " + forecast[i].humidity + "%",
-        // TODO: get icon working
-        "icon: " + forecast[i].weather.icon
-      );
+    }).then(function (responseNow) {
+    // console.log(responseNow);
+    var lat = responseNow.coord.lat;
+    var lon = responseNow.coord.lon;
+    var queryURLall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${APIKey}&units=imperial`;
+    $(".citySearch").val("")
+    $.ajax({
+      url: queryURLall,
+      method: "GET",
+    }).then(function (responseAll) {
+    // variables to hold information for TODAY
+      var cityName = responseNow.name;
+      var dateTodayUnix = responseNow.dt;
+      var unixMS = dateTodayUnix * 1000;
+      var dateTodayObject = new Date(unixMS);
+      var humanDateToday = dateTodayObject.toLocaleDateString("en-US");
+      var tempF = responseAll.current.temp;
+      var humidity = responseAll.current.humidity;
+      var wind = responseAll.current.wind_speed;
+      var uvIndex = responseAll.current.uvi;
+      var icon = $("<img>").attr("src", `http://openweathermap.org/img/wn/${responseNow.weather[0].icon}@2x.png`);
+      var forecast = responseAll.daily;
+      // variables to hold information for 5 day
+      var dateUnixMS;
+      var dateObject; 
+      var humanDate; 
+      var iconForecast
+      var fiveDay; 
+      var dateI; 
+      var minTemp;
+      var maxTemp;
+    // for loop to render information for each day in five day forecast, with values assigned to variables stated prior
+        header = $("<h3 class=header>").text("Five-day forecast")
+        $(".five-day").append(header)  
+      for (var i = 1; i < 6; i++) {
+        dateUnixMS = forecast[i].dt * 1000;
+        dateObject = new Date(dateUnixMS);
+        humanDate = dateObject.toLocaleDateString("en-US");
+        
+        fiveDay = $("<div class='col-2 card-body border dayCard'>")
+        dateI = $("<h5 class='card-title'>").text(`${humanDate}`)
+        iconForecast = $("<img>").attr("src", `http://openweathermap.org/img/wn/${forecast[i].weather[0].icon}@2x.png`)
+        maxTemp = $("<p class='card-text'>").text(`Temp high: ${Math.round(forecast[i].temp.max)}°F`)
+        minTemp = $("<p class='card-text'>").text(`Temp low: ${Math.round(forecast[i].temp.min)}°F`)
+        humidityI = $("<p class='card-text'>").text(`humidity: ${forecast[i].humidity}%`)
+      // appending five day forecast information to cards for each day
+        fiveDay.append(dateI)
+        fiveDay.append(iconForecast)
+        fiveDay.append(minTemp)
+        fiveDay.append(maxTemp)
+        fiveDay.append(humidityI)
+      // appending to the page
+        $(".forecast").append(fiveDay)
+      }
+      // creating divs with today's information and appending to page
+      var cityDate = $("<h1>").text(`${cityName} ${humanDateToday}`)
+      $(".today").append(cityDate)
+      $(".today").append(icon)
+      var tempToday = $("<p>").text(`Temperature: ${tempF} °F`)
+      $(".today").append(tempToday)
+      var humidityToday = $("<p>").text(`Humidity: ${humidity}%`)
+      $(".today").append(humidityToday)
+      var windToday = $("<p>").text(`Wind Speed: ${wind} MPH`)
+      $(".today").append(windToday)
+      var uvToday = $("<p class ='uv'>").text(`UV Index: `)
+      var span = $(`<span>${uvIndex}</span>`)
+      span.appendTo(uvToday)
+      $(".today").append(uvToday)
       
-    }
-    console.log(
-      "City: " + cityName,
-    //   TODO: convert date to standard date
-      "Today's Date: " + dateToday,
-    //   TODO: figure out how to add the "degree" symbol
-      "Temperature: " + (tempF) + " F",
-      "Humidity: " + humidity + "%",
-      "Wind Speed: " + wind + " MPH",
-      "lat: " + lat,
-      "lon: " + lon,
-    //   TODO: 
-      "UV Index: " + uvIndex,
-    //   TODO: get icon working
-      icon
-    );
-  });
+      if(uvIndex < 3){
+        span.addClass("uvFav")
+      }else if(uvIndex >= 3 && uvIndex < 6){
+        span.addClass("uvMod")
+      }else if(uvIndex >= 6){
+        span.addClass("uvSev")
+      }
+    });
 });
+};
+// creates a new button for city being searched and places all recent city search buttons on screen
+function renderBtns(){
+    for (i = 0; i < cities.length; i++){
+        var a =$("<button>");
+        a.addClass("historyBtn btn btn-outline-secondary")
+        a.attr("data-number", i);
+        a.text(cities[i]);
+        $(".prev-search").append(a);
+        localStorage.setItem(("city"), (cities), JSON.stringify(city));
+    }
+}      
+// JSON.parse(localStorage.getItem(cities, "data-number"))
+// Adding a click event listener to "citySearchBtn"
+$(".citySearchBtn").on("click", function (event) {
+    event.preventDefault()
+// FIXME: how do I empty the iput field? If figured out, then do it on all click events. 
+    // prevent adding content multiple times
+    $(".header").empty()
+    $(".today").empty()
+    $(".forecast").empty()
+    $(".prev-search").empty()
+//  defines city being searched for and adds that city to "cities" array for recent search display
+    var city = $(".citySearch").val().trim()
+    cities.push(city)
+    
+// calls functions to retrieve and display info
+    renderBtns()
+    displayWeatherInfo()
+});
+// adding a click event listener to previously searched cities to repopulate the data
+$(document).on("click", ".historyBtn", function (event) {
+  event.preventDefault()
+ // activate proper button from city array for displayWeatherInfo function to work and empties prior data to prevent duplicate
+$(".citySearch").val($(this).text())
+  $(".header").empty()
+  $(".today").empty()
+  $(".forecast").empty()
+  $(".prev-search").empty()
+  renderBtns ()
+  displayWeatherInfo()
+}) 
 
-$(".citySearch").on("click", function(){
-    $(this).val
-})
+// TODO: Local Storage (set and get) Line 99 and 102 
+// TODO: if time, then set a limit of 10 cities by limiting length of cities array. (I know how to do this I think)
+// TODO: if 
+ 
 
 
-
-// function logData(city){
-
-// }
